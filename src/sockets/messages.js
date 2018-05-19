e2b3ad711f6ca29579377dba409830ce56387c52
Jules LAURENT <jules.laurent@ynov.com>
2018-05-19 08:54:25 +0200

    message are now stored in the db

--- src/sockets/messages.js
+++ src/sockets/messages.js
@@ -1,8 +1,13 @@
-module.exports = (socket) =>  {
-    socket.on('msg', function(msg){
+module.exports = (socket,db) =>  {
+    socket.on('msg', function(content){
         //TODO Store message in db.
-        //TODO Broadcast message.
-        console.log('message: ' + msg);
-        socket.emit('msg-bd',msg);
+        //TODO Broadcast message
+        console.log('Received: ' + content);
+        db.Message.create({
+            content,
+            userId : socket.request.session.full_user.id
+        }).then((r) => {
+            socket.emit('msg-bd',content);
+        });
     });
 };
